9120b55beb18c4973ffe22b44b5f5f29aff73124
Till Prochaska <mail@tillprochaska.de>
2018-09-15 18:46:19 +0200

    Remove unecessary files

--- test/support/bootstrap.php
+++ test/support/bootstrap.php
@@ -1,17 +1,16 @@
 <?php
 
 namespace TillProchaska\ApiHelpers;
-require_once 'index.php';
 
 class KirbyTestCase extends \PHPUnit\Framework\TestCase {
 
-	public function kirby() {
-		$base = dirname(__DIR__);
-		return new \Kirby([
-		    'roots' => [
-		        'base'     => $base,
-		        'content'  => $base . '/fixtures/content',
-		    ]
-		]);
-	}
+    public function kirby() {
+        $base = dirname(__DIR__);
+        return new \Kirby([
+            'roots' => [
+                'base'     => $base,
+                'content'  => $base . '/fixtures/content',
+            ]
+        ]);
+    }
 }
